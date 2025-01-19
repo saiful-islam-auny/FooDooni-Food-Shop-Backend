e9f2752ef75e9ccd34e35b99f87e9952f617a85b
Saiful Islam Auny <aunychowdhury99@gmail.com>
2025-01-20 00:08:57 +0600

    netlify link added

--- foodshop_api/settings.py
+++ foodshop_api/settings.py
@@ -174,6 +174,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5500",
     "http://127.0.0.1:5501",
+    "https://foodoni.netlify.app",
 ]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
